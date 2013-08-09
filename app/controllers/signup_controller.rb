08614d080a95ff85fca11a59ceb86dbdd0657a12
maisaengineering <contact@maisasolutions.com>
2013-08-09 19:21:53 +0530

    changed format to json

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -1,24 +1,19 @@
 class SignupController < ApplicationController
   def new
-     @signup = Signup.new
-     respond_to do |format|
+    @signup = Signup.new
+    respond_to do |format|
       format.html # new.html.erb
       format.json { render json: @signup }
-     end
+    end
   end
 
   def create
-     @signup = Signup.new(params[:signup])
+    @signup = Signup.new(email: params["email"], zipcode: params["zipcode"])
 
-     respond_to do |format|
-      if @signup.save
-        format.html { redirect_to root_url, notice: 'successfully created'  }
-        format.json { render json: @signup, status: :created, location: @signup }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @signup.errors, status: :unprocessable_entity }
-      end
-     end
-
+    if @signup.save!
+      render :json => { :message => 'Ok' }, content_type: "application/json"
+    else
+      render :json => { :message => 'Not Ok' }, :status => 422
+    end
   end
 end
